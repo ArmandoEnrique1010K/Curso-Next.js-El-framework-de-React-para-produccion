a12987ce708b82983de9bef3fc1ec8471b13cd40
ArmandoEnrique1010K <enrique1010k@gmail.com>
2026-01-07 23:42:02 -0500

    24

--- 01-first-steps/components/navbar/Navbar.tsx
+++ 01-first-steps/components/navbar/Navbar.tsx
@@ -1,27 +1,33 @@
-const temporalAsync = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true);
-    }, 2000)
-  })
-}
+import { HomeIcon } from "@primer/octicons-react";
+import Link from "next/link";
+import { ActiveLink } from "../active-link/ActiveLink";
+
+const navItems = [
+  { path: '/about', text: 'About'},
+  { path: '/pricing', text: 'Pricing'},
+  { path: '/contact', text: 'Contact'},
+]
+
 
 export default async function Navbar() {
-
-  await temporalAsync();
-  
+ 
   return (
     // La opacidad en tailwind v4 se especifica colocando un / luego 
     // de un color de fondo seguido del nivel de opacidad (0 a 100)
     <nav className="flex bg-blue-800/30 p-2 m-2 rounded-md">
       
-      <span>Home</span>
+      <Link href={'/'} className="flex items-center">
+        <HomeIcon size={20} className="mr-2"/>
+        <span>Home</span>
+      </Link>
 
       <div className="flex flex-1"></div>
 
-        <a className="mr-2" href="/about">About</a>
-        <a className="mr-2" href="/pricing">Pricing</a>
-        <a className="mr-2" href="/contact">Contact</a>
+        {
+          navItems.map(navItem => (
+            <ActiveLink key={navItem.path} {...navItem} />
+          ))
+        }
     </nav>
   );
 }
