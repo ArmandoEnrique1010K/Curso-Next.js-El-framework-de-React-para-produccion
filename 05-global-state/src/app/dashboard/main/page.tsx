ad6ee6ff1c88314ec8339a61e0ca3a849280bd8a
ArmandoEnrique1010K <enrique1010k@gmail.com>
2026-01-18 00:01:47 -0500

    79

---
+++ 05-global-state/src/app/dashboard/main/page.tsx
@@ -0,0 +1,14 @@
+import { SimpleWidget } from "@/app/components";
+
+export default function page() {
+  return (
+    <div className="text-black p-2">
+      <h1 className="mt-2 text-3xl">Dashboard</h1>
+      <span className="text-xl">Información general</span>
+
+      <div className="flex flex-wrap p-2 items-center justify-center">
+        <SimpleWidget />
+      </div>
+    </div>
+  )
+}
---
+++ 05-global-state/src/shopping-cart/components/CartCounter.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useState } from 'react'
+
+interface Props {
+    value?: number;
+}
+
+export const CartCounter = ({value = 0}: Props) => {
+
+    const [counter, setCounter] = useState(value);
+
+    const handleChangeCounter = (value: number) => {
+        setCounter(c => c + value)
+    }
+
+
+    return (
+        <>
+            <span className='text-9xl'>{counter}</span>
+
+            <div className='flex'>
+                <button
+                    className='flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2 cursor-pointer'
+                    onClick={() => handleChangeCounter(+1)}
+                >+1</button>
+
+                <button
+                    className='flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] ml-2 cursor-pointer'
+                    onClick={() => handleChangeCounter(-1)}
+                >-1</button>
+            </div>
+
+        </>
+    )
+}
