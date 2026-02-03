80f07e35ff473114c9d3fac6e0b08d28d019736e
ArmandoEnrique1010K <enrique1010k@gmail.com>
2026-02-02 21:19:53 -0500

    seccion 10

---
+++ 10-admin-todos-next-restapi/src/app/api/seed/route.ts
@@ -0,0 +1,28 @@
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: Request) {
+
+    await prisma.todo.deleteMany();
+
+    // const todo = await prisma.todo.create({
+    //     data: {
+    //         description: 'Piedra del alma', complete: true,
+    //     }
+    // })
+    // console.log(todo);
+
+    await prisma.todo.createMany({
+        data: [
+            { description: 'Piedra del alma', complete: true },
+            { description: 'Piedra del poder', complete: true },
+            { description: 'Piedra del tiempo', complete: true },
+            { description: 'Piedra del espacio', complete: true },
+            { description: 'Piedra de la realidad', complete: true },
+        ]
+    })
+
+    return NextResponse.json({
+        message: 'Seed',
+    })
+}
