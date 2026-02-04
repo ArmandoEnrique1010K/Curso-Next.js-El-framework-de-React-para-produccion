299db5da06f170b8f0080e7d8b827ab71de61399
ArmandoEnrique1010K <enrique1010k@gmail.com>
2026-02-03 21:43:26 -0500

    Corregir el proyecto de la sesion 10

--- 10-admin-todos-next-restapi/src/app/dashboard/page.tsx
+++ 10-admin-todos-next-restapi/src/app/dashboard/page.tsx
@@ -1,17 +1,13 @@
-import WidgetItem from '@/components/WidgetItem'
-import React from 'react'
+import { WidgetItem } from "@/components";
 
 export default function DashboardPage() {
-    return (
-        <>
-            <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+  return (
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
 
-                {/* TODO: src/components <WidgetItem /> */}
-                <WidgetItem/>
-                {/* TODO: Fin <WidgetItem /> */}
 
-            </div>
+      <WidgetItem />
+      
 
-        </>
-    )
+    </div>
+  );
 }
--- 10-admin-todos-next-restapi/src/components/SidebarItem.tsx
+++ 10-admin-todos-next-restapi/src/components/SidebarItem.tsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import { CiBookmarkCheck } from 'react-icons/ci'
+'use client';
 
-export default function SidebarItem() {
-    return (
-        <>
-            {/* Active className: text-white bg-gradient-to-r from-sky-600 to-cyan-400 */}
-            <li>
-                <a href="#" className="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-linear-to-r from-sky-600 to-cyan-400">
-                    <CiBookmarkCheck size={30} />
-                    <span className="-mr-1 font-medium">Dashboard</span>
-                </a>
-            </li>
-            <li>
-                <a href="#" className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-                    <CiBookmarkCheck size={30} />
-                    <span className="group-hover:text-gray-700">Categories</span>
-                </a>
-            </li>
-        </>
-    )
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+interface Props {
+  icon:  React.ReactNode;
+  path:  string;
+  title: string;
+}
+
+
+export const SidebarItem = ({ icon, path, title }: Props ) => {
+
+  const pathName = usePathname();
+
+
+  {/* Active className: text-white bg-gradient-to-r from-sky-600 to-cyan-400 */}
+  return (
+    <li>
+      <Link href={ path } className={`
+        px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group
+        hover:bg-linear-to-r hover:bg-sky-600 hover:text-white
+        ${ path === pathName ? 'text-white bg-linear-to-r from-sky-600 to-cyan-400': '' }
+      `}>
+        { icon }
+        <span className="group-hover:text-white-700">{ title }</span>
+      </Link>
+    </li>
+  )
 }
