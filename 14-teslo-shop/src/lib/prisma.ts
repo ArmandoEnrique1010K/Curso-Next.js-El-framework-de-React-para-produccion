50ed9b22f83f2dcc794ac3032a709401bacf4fe4
ArmandoEnrique1010K <enrique1010k@gmail.com>
2026-02-08 10:18:38 -0500

    Volver configurar Next Auth

--- 14-teslo-shop/src/lib/prisma.ts
+++ 14-teslo-shop/src/lib/prisma.ts
@@ -1,17 +1,42 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+let prisma: PrismaClient;
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient({ adapter });
+} else {
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient({ adapter });
+  }
+  prisma = (global as any).prisma;
 }
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+export default prisma;
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
-}
+// CONFIGURACION ANTIGUA
+// import { PrismaClient } from "@prisma/client"
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
 
-export default prisma
+// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClientSingleton | undefined
+// }
+
+// const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+// export default prisma
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
