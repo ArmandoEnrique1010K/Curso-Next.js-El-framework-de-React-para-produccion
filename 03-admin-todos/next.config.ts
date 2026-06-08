a9ae56283c2136553cfc14f03e36120dcb993c89
Armando KD <enrique1010k@gmail.com>
2026-06-08 16:14:28 -0500

    restaure el tercer proyecto

---
+++ 03-admin-todos/next.config.ts
@@ -0,0 +1,26 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "tailus.io",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
